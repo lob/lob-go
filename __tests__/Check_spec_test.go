152e9a31fcdd801bdf70be15dc6a7b15d1064463
BennyKitchell <bennykitchell@gmail.com>
2022-11-29 11:15:34 -0600

    feat: add tests for checks

--- __tests__/Check_spec_test.go
+++ __tests__/Check_spec_test.go
@@ -3,8 +3,10 @@ package tests
 import (
 	"context"
 	"os"
+	"testing"
 
 	lob "github.com/lob/lob-go"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -44,106 +46,104 @@ func (suite *CheckTestSuite) SetupTest() {
 
 	verifyAmounts := []int32{11, 35}
 	suite.mockVerify = *lob.NewBankAccountVerify(verifyAmounts)
+	suite.apiClient.BankAccountsApi.BankAccountVerify(suite.ctx, suite.mockAccount.Id).BankAccountVerify(suite.mockVerify).Execute()
 
 	suite.addressEditableList = CreateAddressesEditableList()
-	suite.checkEditable = *lob.NewCheckEditable(suite.addressEditableList[0], string("adr_12345"), *lob.NewNullableString(&suite.mockAccount.Id), 100)
+	suite.checkEditable = *lob.NewCheckEditable(suite.addressEditableList[0], suite.addressEditableList[1], *lob.NewNullableString(&suite.mockAccount.Id), 100)
 }
 
-// func (suite *CheckTestSuite) TestCheckCreate() {
-// 	t := suite.T()
-// 	resp, _, err := suite.apiClient.AddressesApi.AddressCreate(suite.ctx).AddressEditable(suite.addressEditableList[0]).Execute()
-// 	assert.Nil(t, err)
-// 	if assert.NotNil(t, resp) {
-// 		assert.Equal(t, suite.addressEditableList[0].AddressLine1, resp.AddressLine1)
-// 		suite.createdAddress.Id = resp.Id
-// 	}
-// }
+func (suite *CheckTestSuite) TestCheckCreate() {
+	t := suite.T()
+	resp, _, err := suite.apiClient.ChecksApi.CheckCreate(suite.ctx).CheckEditable(suite.checkEditable).Execute()
 
-// func (suite *AddressTestSuite) TestAddressCreateBadParameter() {
-// 	t := suite.T()
-// 	_, _, err := suite.apiClient.AddressesApi.AddressCreate(suite.ctx).AddressEditable(suite.addressEditableList[6]).Execute()
-// 	if assert.NotNil(t, err) {
-// 		assert.Equal(t, "422 Unprocessable Entity", err.Error())
-// 	}
-// }
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp) {
+		assert.NotNil(t, resp.Id)
+		assert.Equal(t, suite.checkEditable.Amount, resp.Amount)
+	}
+}
 
-// func (suite *AddressTestSuite) TestAddressCreateBadApiKey() {
-// 	t := suite.T()
-// 	_, _, err := suite.apiClient.AddressesApi.AddressCreate(suite.badctx).AddressEditable(suite.addressEditableList[0]).Execute()
-// 	if assert.NotNil(t, err) {
-// 		assert.Equal(t, "401 Unauthorized", err.Error())
-// 	}
-// }
+func (suite *CheckTestSuite) TestCheckCreateBadApiKey() {
+	t := suite.T()
+	_, _, err := suite.apiClient.ChecksApi.CheckCreate(suite.badctx).CheckEditable(suite.checkEditable).Execute()
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "401 Unauthorized", err.Error())
+	}
+}
 
-// func (suite *AddressTestSuite) TestAddressRetrieve() {
-// 	t := suite.T()
-// 	resp, _, err := suite.apiClient.AddressesApi.AddressRetrieve(suite.ctx, *suite.createdAddress.Id).Execute()
-// 	assert.Nil(t, err)
-// 	if assert.NotNil(t, resp) {
-// 		assert.Equal(t, resp.Id, suite.createdAddress.Id)
-// 	}
-// }
+func (suite *CheckTestSuite) TestCheckRetrieve() {
+	t := suite.T()
+	createdCheck, _, _ := suite.apiClient.ChecksApi.CheckCreate(suite.ctx).CheckEditable(suite.checkEditable).Execute()
 
-// func (suite *AddressTestSuite) TestAddressRetrieveBadApiKey() {
-// 	t := suite.T()
-// 	_, _, err := suite.apiClient.AddressesApi.AddressRetrieve(suite.badctx, *suite.createdAddress.Id).Execute()
-// 	if assert.NotNil(t, err) {
-// 		assert.Equal(t, "401 Unauthorized", err.Error())
-// 	}
-// }
+	resp, _, err := suite.apiClient.ChecksApi.CheckRetrieve(suite.ctx, createdCheck.Id).Execute()
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp) {
+		assert.Equal(t, resp.Id, createdCheck.Id)
+	}
+}
 
-// func (suite *AddressTestSuite) TestAddressList() {
-// 	t := suite.T()
-// 	resp, _, err := suite.apiClient.AddressesApi.AddressesList(suite.ctx).Execute()
-// 	assert.Nil(t, err)
-// 	if assert.NotNil(t, resp) {
-// 		assert.Greater(t, resp.GetCount(), int32(0))
-// 	}
-// }
+func (suite *CheckTestSuite) TestCheckRetrieveBadApiKey() {
+	t := suite.T()
+	_, _, err := suite.apiClient.ChecksApi.CheckRetrieve(suite.badctx, "fake id").Execute()
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "401 Unauthorized", err.Error())
+	}
+}
 
-// func (suite *AddressTestSuite) TestAddressListWithIncludeParameter() {
-// 	t := suite.T()
-// 	resp, _, err := suite.apiClient.AddressesApi.AddressesList(suite.ctx).Limit(3).Execute()
-// 	assert.Nil(t, err)
-// 	if assert.NotNil(t, resp) {
-// 		assert.Equal(t, resp.GetCount(), int32(3))
-// 	}
-// }
+func (suite *CheckTestSuite) TestCheckList() {
+	t := suite.T()
+	resp, _, err := suite.apiClient.ChecksApi.ChecksList(suite.ctx).Execute()
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp) {
+		assert.Greater(t, resp.GetCount(), int32(0))
+	}
+}
 
-// func (suite *AddressTestSuite) TestAddressListWithNextPageToken() {
-// 	t := suite.T()
-// 	firstResponse, _, firstErr := suite.apiClient.AddressesApi.AddressesList(suite.ctx).Limit(1).Execute()
-// 	assert.Nil(t, firstErr)
-// 	responeAfter, _, err := suite.apiClient.AddressesApi.AddressesList(suite.ctx).After(firstResponse.GetNextPageToken()).Execute()
-// 	assert.Nil(t, err)
-// 	if assert.NotNil(t, responeAfter) {
-// 		assert.Greater(t, responeAfter.GetCount(), int32(0))
-// 	}
-// }
+func (suite *CheckTestSuite) TestCheckListWithIncludeParameter() {
+	t := suite.T()
+	resp, _, err := suite.apiClient.ChecksApi.ChecksList(suite.ctx).Limit(3).Execute()
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp) {
+		assert.Equal(t, resp.GetCount(), int32(3))
+	}
+}
 
-// func (suite *AddressTestSuite) TestAddressListWithPrevPageToken() {
-// 	t := suite.T()
-// 	firstResponse, _, firstErr := suite.apiClient.AddressesApi.AddressesList(suite.ctx).Limit(1).Execute()
-// 	assert.Nil(t, firstErr)
-// 	responeAfter, _, errAfter := suite.apiClient.AddressesApi.AddressesList(suite.ctx).After(firstResponse.GetNextPageToken()).Execute()
-// 	assert.Nil(t, errAfter)
-// 	responseBefore, _, err := suite.apiClient.AddressesApi.AddressesList(suite.ctx).Before(responeAfter.GetPrevPageToken()).Execute()
-// 	assert.Nil(t, err)
-// 	if assert.NotNil(t, responseBefore) {
-// 		assert.Greater(t, responseBefore.GetCount(), int32(0))
-// 	}
-// }
+func (suite *CheckTestSuite) TestCheckListWithNextPageToken() {
+	t := suite.T()
+	firstResponse, _, firstErr := suite.apiClient.ChecksApi.ChecksList(suite.ctx).Limit(1).Execute()
+	assert.Nil(t, firstErr)
+	responeAfter, _, err := suite.apiClient.ChecksApi.ChecksList(suite.ctx).After(firstResponse.GetNextPageToken()).Execute()
+	assert.Nil(t, err)
+	if assert.NotNil(t, responeAfter) {
+		assert.Greater(t, responeAfter.GetCount(), int32(0))
+	}
+}
 
-// func (suite *AddressTestSuite) TestAddressDelete() {
-// 	t := suite.T()
-// 	resp, _, err := suite.apiClient.AddressesApi.AddressDelete(suite.ctx, *suite.createdAddress.Id).Execute()
-// 	assert.Nil(t, err)
-// 	if assert.NotNil(t, resp) {
-// 		assert.Equal(t, resp.Id, suite.createdAddress.Id)
-// 	}
-// }
+func (suite *CheckTestSuite) TestCheckListWithPrevPageToken() {
+	t := suite.T()
+	firstResponse, _, firstErr := suite.apiClient.ChecksApi.ChecksList(suite.ctx).Limit(1).Execute()
+	assert.Nil(t, firstErr)
+	responeAfter, _, errAfter := suite.apiClient.ChecksApi.ChecksList(suite.ctx).After(firstResponse.GetNextPageToken()).Execute()
+	assert.Nil(t, errAfter)
+	responseBefore, _, err := suite.apiClient.ChecksApi.ChecksList(suite.ctx).Before(responeAfter.GetPrevPageToken()).Execute()
+	assert.Nil(t, err)
+	if assert.NotNil(t, responseBefore) {
+		assert.Greater(t, responseBefore.GetCount(), int32(0))
+	}
+}
 
-// // Runs all of the tests
-// func TestAddressTestSuite(t *testing.T) {
-// 	suite.Run(t, new(AddressTestSuite))
-// }
+func (suite *CheckTestSuite) TestCheckCancel() {
+	t := suite.T()
+	createdCheck, _, _ := suite.apiClient.ChecksApi.CheckCreate(suite.ctx).CheckEditable(suite.checkEditable).Execute()
+
+	resp, _, err := suite.apiClient.ChecksApi.CheckCancel(suite.ctx, createdCheck.Id).Execute()
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp) {
+		assert.Equal(t, resp.GetId(), createdCheck.Id)
+	}
+}
+
+// Runs all of the tests
+func TestCheckTestSuite(t *testing.T) {
+	suite.Run(t, new(CheckTestSuite))
+}
